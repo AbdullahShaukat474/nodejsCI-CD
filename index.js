7f056a8e43f610baeac249242f6e4aef355fb166
root <root@ip-172-31-24-247.ec2.internal>
2025-08-06 12:44:19 +0000

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+//Root route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
