ffba34c9dedde06cfd11c4b2ad1cd8cdb071e359
root <root@ip-172-31-24-247.ec2.internal>
2025-08-07 05:55:18 +0000

    in

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 //Root route
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World v1');
 });
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
